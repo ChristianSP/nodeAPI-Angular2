0c2bcb585a1c87ca5d1565c2a31dfa445e831555
ChristianSP <christian.jacodelfi.24@gmail.com>
2017-01-29 16:18:52 +0100

    Mail confirmation

---
+++ nodeAPI/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'secret': 'm1SkeoP02w4p4',
+    'database': 'mongodb://localhost:27017/wordsDP',
+    'userMail': 'nodeAngularApp@gmail.com',
+    'passMail': 'qwertyNode'
+
+};
