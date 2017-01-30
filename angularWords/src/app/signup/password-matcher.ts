da3192508cd3ce2f198fc843b91e9b91275dbed9
ChristianSP <christian.jacodelfi.24@gmail.com>
2017-01-30 01:05:59 +0100

    new gh-pages version

--- angularWords/src/app/signup/password-matcher.ts
+++
@@ -1,8 +0,0 @@
-import {AbstractControl} from '@angular/forms'
-
-export const passwordMatcher = (control: AbstractControl): {[key: string]: boolean} => {
-      const pass = control.get('password');
-        const confirm = control.get('confirmPassword');
-        if (!pass || !confirm) return null;
-        return pass.value === confirm.value ? null : { nomatch: true };
-      };
